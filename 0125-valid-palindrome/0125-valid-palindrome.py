e43076a0bbe5f275d3f51ccf0ef095ba02234756
IshsGit <aishtahad@yahoo.com>
2024-03-01 22:47:33 -0500

    Time: 46 ms (53.41%), Space: 21.6 MB (14.60%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Convert the string to lowercase and remove non-alphanumeric characters
+        cleaned_s = ''.join(char.lower() for char in s if char.isalnum())
+        
+        # Check if the cleaned string is equal to its reverse
+        return cleaned_s == cleaned_s[::-1]
